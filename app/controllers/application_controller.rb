3915877a736a539798db453863eddba0ba5a6d10
Benjamin Shyman <bshyman@gmail.com>
2022-08-04 21:21:59 -0500

    pokeballs and landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-  
+  helper_method :current_user, :current_player
+
+  before_action :authorize_player, except: :landing
+
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.first
   end
-  
-  def landing
-    render 'layouts/index'
+
+  def current_player
+    @current_player ||= Player.find_by(id: session[:player_id])
   end
+
+  def authorize_player
+    redirect_to landing_path unless current_player.present?
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def sign_in
+    render 'sign-in'
+  end
   def index
   end
 end
