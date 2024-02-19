7ea011f868d1f0c23d17145937cd0c974b834850
Benjamin Shyman <bshyman@gmail.com>
2024-02-18 21:12:26 -0600

    user index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  def level
+    xp / 100 + 1
+  end
+
+  def level_progress
+    xp % 100
+  end
+
+  def name
+    [first_name, last_name].compact.join(' ')
+  end
+
+  def age
+    return 'N/A' unless birthday
+
+    today = Date.today
+    age = today.year - birthday.year
+    age -= 1 if today < birthday + age.years
+    age
+  end
 end
