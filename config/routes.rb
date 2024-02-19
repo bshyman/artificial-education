f0a9efe0c0b77e0fb5e4fe83769264ae6aa6a745
Benjamin Shyman <bshyman@gmail.com>
2024-02-18 20:48:11 -0600

    pokedex

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :pokemon, except: :destroy do
     get 'new_question' => 'pokemon#new_question', on: :collection
+    get 'pokedex', on: :collection
   end
   resources :games, only: [:index] do
     get 'new_pokemon_round', to: 'rounds#pokemon_new'
